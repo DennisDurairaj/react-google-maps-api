6ead83afe67af347c3b83961b78cd1bde7563f64
Dennis Durairaj <dennis.durairaj@gmail.com>
2017-11-10 18:26:20 +0100

    Add Semantic UI and fix package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to React</h1>
+      <div className="ui container">
+        <Route path="/" exact component={Home} />
       </div>
     );
   }
